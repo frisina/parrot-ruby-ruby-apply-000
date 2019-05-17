e7ebc02b8ee2380fd3a61abd9b7285f67590d851
Marco Frisina <marco@frisina.xyz>
2019-05-17 13:22:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase)
+def parrot(phrase = "Sqawk!")
   phrase = phrase.to_s
   return phrase
   puts phrase
